2f4cce354ca8b354b7eb0650dc2c22d3b4c87dad
Aaron Goidel <acgoidel@gmail.com>
2021-05-25 18:50:17 -0400

    resume and contact copy

--- pages/index.js
+++ pages/index.js
@@ -55,24 +55,16 @@ export default function Index() {
         </section>
 
         <section>
+          <h1>Resume & Contact</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            Want a copy of my resume?
           </p>
+          <button>
+            <p style={{margin: 0, color: "white"}}>Download It!</p>
+          </button>
 
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-            culpa qui officia deserunt mollit anim id est laborum.
+            You can find me on GitHub and LinkedIn
           </p>
         </section>
       </main>
