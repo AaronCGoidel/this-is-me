651d28e0cd59b8908934a9c64cb3ef5e2e1ff513
Aaron Goidel <acgoidel@gmail.com>
2021-05-28 18:41:24 -0400

    meta info

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,18 @@ export default function Index(props) {
     <>
       <Head>
         <title>Aaron Goidel</title>
+        <meta name="title" content="Aaron Goidel" />
+        <meta
+          name="description"
+          content="Aaron is a passionate Computer Science student, software developer, and creative."
+        />
+        <meta
+          name="keywords"
+          content="web dev, developer, programmer, New York, Toronto, Toronto developer, NYC developer"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
       </Head>
       <Home />
       <main>
@@ -20,65 +32,62 @@ export default function Index(props) {
         </header>
         <div className={"container"}>
           <section>
-              <h1>About Me</h1>
-              <p>
-                I am an intensely passionate student, currently studying
-                Computer Science and Linguistics at the University of Toronto. I
-                work part-time as a backend engineer and am always tinkering and
-                reading. I am passionate about software as the tool to build a
-                better future.
-              </p>
+            <h1>About Me</h1>
+            <p>
+              I am an intensely passionate student, currently studying Computer
+              Science and Linguistics at the University of Toronto. I work
+              part-time as a backend engineer and am always tinkering and
+              reading. I am passionate about software as the tool to build a
+              better future.
+            </p>
 
-              <p>
-                My main interests in school are in computational linguistics as
-                well as languages and compilers. I am a Technical Officer in the
-                Computer Science Student Community, helping students to bridge
-                the gap between theory and implementation by mentoring them
-                during the development process.
-              </p>
+            <p>
+              My main interests in school are in computational linguistics as
+              well as languages and compilers. I am a Technical Officer in the
+              Computer Science Student Community, helping students to bridge the
+              gap between theory and implementation by mentoring them during the
+              development process.
+            </p>
 
-              <p>
-                Out of school, I have experience developing production software
-                in teams small to large. I enjoy making small web projects as
-                well as exploring many other areas of computer science. These
-                range from language design, to physics simulations, to
-                blockchain, and beyond. I have a love of all things food and
-                cooking and I am always listening to or making music.
-              </p>
+            <p>
+              Out of school, I have experience developing production software in
+              teams small to large. I enjoy making small web projects as well as
+              exploring many other areas of computer science. These range from
+              language design, to physics simulations, to blockchain, and
+              beyond. I have a love of all things food and cooking and I am
+              always listening to or making music.
+            </p>
           </section>
           <section className="right" id={"projects"}>
-              <h1>My Projects</h1>
-              <p>
-                Whether for school or work, for a hackathon, or just for fun, I
-                am constantly working on projects I find interesting. Here are
-                just a few of my favorites.
-              </p>
-              <motion.div
-                transition={{ staggerChildren: 0.1 }}
-                className="cards"
-              >
-                {props.projects.map((project, i) => {
-                  return <ProjectCard key={i} num={i} {...project} />;
-                })}
-              </motion.div>
+            <h1>My Projects</h1>
+            <p>
+              Whether for school or work, for a hackathon, or just for fun, I am
+              constantly working on projects I find interesting. Here are just a
+              few of my favorites. Click around for details.
+            </p>
+            <motion.div transition={{ staggerChildren: 0.1 }} className="cards">
+              {props.projects.map((project, i) => {
+                return <ProjectCard key={i} num={i} {...project} />;
+              })}
+            </motion.div>
           </section>
 
           <section>
-              <h1>Resume & Contact</h1>
-              <p>Want a copy of my resume?</p>
-              <a href="resume.pdf" download="Aaron Goidel Resume.pdf">
-                <motion.button whileHover={{ scale: 1.05 }}>
-                  <p style={{ margin: 0 }}>Download It! </p>
-                </motion.button>
-              </a>
+            <h1>Resume & Contact</h1>
+            <p>Want a copy of my resume?</p>
+            <a href="resume.pdf" download="Aaron Goidel Resume.pdf">
+              <motion.button whileHover={{ scale: 1.05 }}>
+                <p style={{ margin: 0 }}>Download It! </p>
+              </motion.button>
+            </a>
 
-              <p>You can find me online at the following places</p>
-              <a href={"https://linkedin.com/in/aaroncgoidel"} target="_blank">
-                <FaLinkedin size={35} />
-              </a>
-              <a href={"https://github.com/aaroncgoidel"} target="_blank">
-                <FiGithub size={35} />
-              </a>
+            <p>You can find me online at the following places</p>
+            <a href={"https://linkedin.com/in/aaroncgoidel"} target="_blank">
+              <FaLinkedin size={35} />
+            </a>
+            <a href={"https://github.com/aaroncgoidel"} target="_blank">
+              <FiGithub size={35} />
+            </a>
           </section>
         </div>
       </main>
