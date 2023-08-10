713a3bbdfa3b220d1217262cb935d9c72106f7e5
Aaron Goidel <acgoidel@gmail.com>
2023-08-10 00:09:51 -0400

    add project cards

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { FaLinkedin } from "react-icons/fa";
 import Layout from "../components/Layout";
 import Head from "next/head";
 import { useEffect, useState, useRef } from "react";
-// import {ProjectCard} from '../components/ProjectCard';
+import ProjectCard from '../components/ProjectCard';
 import ChatApp from "../components/Chat/ChatApp";
 export default function Index(props) {
   const [offsetY, setOffsetY] = useState(0);
@@ -79,6 +79,32 @@ export default function Index(props) {
             ullamcorper vulputate in at odio.
           </p>
         </div>
+
+        <div id="projects" className="px-4 py-16 relative">
+          <h2 className="text-4xl mb-4 font-bold">Projects</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {[1, 2, 3, 4, 5].map((project) => ( 
+              <ProjectCard key={project} 
+              title="Project Title"
+              description="In est commodo laborum sint magna culpa quis duis. Voluptate id proident voluptate dolore enim sint amet dolore irure Lorem nulla enim exercitation."
+              image="/images/cookie.webp"
+              link="https://google.com"
+              ghLink="AaronCGoidel/this-is-me"
+              />
+              ))}
+              {[1, 2].map((project) => ( 
+              <ProjectCard key={project} 
+              title="Project Title"
+              description="In est commodo laborum sint magna culpa quis duis. Voluptate id proident voluptate dolore enim sint amet dolore irure Lorem nulla enim exercitation."
+              image="/images/cookie.webp"
+              ghLink="AaronCGoidel/this-is-me"
+              />
+              ))}
+          </div>
+        </div>
+
+
+
       </Layout>
     </>
   );
