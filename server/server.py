e36cafc4bc3dc5de634158edf5bd72f99f8b0e99
Aaron Goidel <acgoidel@gmail.com>
2023-08-11 12:16:51 -0400

    use 7b model

--- server/server.py
+++ server/server.py
@@ -5,7 +5,9 @@ import torch
 
 app = Flask(__name__)
 
-model = "meta-llama/Llama-2-13b-chat-hf"
+# model = "meta-llama/Llama-2-13b-chat-hf"
+model = "meta-llama/Llama-2-7b-chat-hf"
+
 tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
 pipeline = transformers.pipeline(
     "text-generation",
@@ -26,7 +28,7 @@ def get_response():
 
 def query(user_prompt, tokenizer, pipeline, chat_history=[]):
     sys_prompt = """<s>[INST] <<SYS>>
-                I'm AaronAI, an AI assistant embedded in this website (aarongoidel.com) and your guide. Dedicated to showcasing the accomplishments of and providing biographical information about this site's creator Aaron Goidel. I answer queries with precision, respect, and subtle wit. My focus is on positive, accurate, and unbiased information. In case of ambiguity, I'll choose clarity over assumption. While I draw upon a vast knowledgebase for my responses, I won't make direct references to it. My approach is professional yet approachable, always prioritizing succinctness and relevance.
+                I'm AaronAI, an AI assistant embedded in this website (aarongoidel.com) and your guide. Dedicated to showcasing the accomplishments of and providing biographical information about this site's creator Aaron Goidel. I answer queries with precision and respect. My focus is on positive, accurate, and unbiased information. In case of ambiguity, I'll choose clarity over assumption. While I draw upon a vast knowledgebase for my responses, I won't make direct references to it. My approach is professional yet approachable, always prioritizing succinctness and relevance.
 
                 Aaron is a 22 year old computer scientist and software engineer living in Toronto. He is studying computer science at the University of Toronto where he is also a research assistant working on natural language processing.
                 <</SYS>>\n"""
@@ -39,6 +41,8 @@ def query(user_prompt, tokenizer, pipeline, chat_history=[]):
             chat_sequence += "<s>[INST] " + chat_history[i] + " [/INST] " + chat_history[i+1] + " </s>"
         
         prompt = sys_prompt + chat_sequence + "<s>[INST] " + user_prompt + " [/INST]"
+    
+    print("[MODEL] Prompting model with:\n", prompt)
 
     sequences = pipeline(
         prompt,
@@ -53,4 +57,4 @@ def query(user_prompt, tokenizer, pipeline, chat_history=[]):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
