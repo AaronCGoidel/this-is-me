bb2e9f0a0edb52a144d81e53f20ac37a7e5d900f
Aaron Goidel <acgoidel@gmail.com>
2023-08-11 15:11:44 -0400

    add fine print for chat

--- components/sections/Chat.tsx
+++ components/sections/Chat.tsx
@@ -13,6 +13,10 @@ const Chat = (): JSX.Element => {
         </div>
         <ChatApp />
       </div>
+      {/* add note */}
+      <div className="flex flex-col justify-center items-center md:flex-row">
+        <p className="text-xs text-gray-500 mt-3">*This chatbot may provide inaccurate information, break character, fail to finish a thought, or otherwise behave unexpectedly. It currently has highly limited knowledge of my background but is being updated regularly.</p>
+        </div>
     </div>
   );
 };
