2780b04ef986960428b176d2d7955e2719a4104f
Aaron Goidel <acgoidel@gmail.com>
2023-09-07 12:40:48 -0400

    vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
-import React from 'react';
-import '../styles/global.css'
+import React from "react";
+import "../styles/global.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-    <Component {...pageProps} />
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
   );
 };
 
